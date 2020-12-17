39839b0d9f20b38c1314ae3a6c56505f8d81f9ac
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-12-17 12:02:41 +0100

    Add of a function to add a user in the database in the UserController.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -48,5 +49,21 @@ class UserController extends AbstractController
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
+    }
+
+    /**
+     * @Route("/users", name="add_user", methods={"POST"})
+     */
+    public function addUser(Request $request)
+    {
+        $data = $request->getContent();
+        $user = $this->get('serializer')
+            ->deserialize($data, 'App\Entity\User', 'json');
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return new Response('', Response::HTTP_CREATED);
     }
 }
