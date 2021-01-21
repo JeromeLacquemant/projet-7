91bf7431f2bb527711f1d12e269c2b463d683c1d
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-01-21 12:04:33 +0100

    Creation of controller, interface and class to delete one user

---
+++ src/Services/UserDelete.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\UserDeleteInterface;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserDelete implements UserDeleteInterface
+{
+    private $userRepository;
+    private $entityManagerInterface;
+
+    public function __construct(
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManagerInterface) 
+    {
+        $this->userRepository = $userRepository;
+        $this->entityManagerInterface = $entityManagerInterface;
+    }
+
+    public function deleteUser($id)
+    {
+        $user = $this->userRepository->find($id);
+
+        $this->entityManagerInterface->remove($user);
+        return $this->entityManagerInterface->flush();
+    }
+}
