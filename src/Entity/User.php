791ad2796566bc4574895be95e79070ede290d67
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-01-22 15:50:20 +0100

    Creation of controller, interface and class to modify a user + Modification of class User because of a mistake about the username

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,7 +40,7 @@ class User
 
     public function getUsername(): ?string
     {
-        return $this->email;
+        return $this->username;
     }
 
     public function setUsername(string $username): self
