647c4085c6773b96145c0dea87e7b79bb711bd50
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-02-07 16:32:41 +0100

    Implementing tests for UserDeleteController. OK.

---
+++ tests/Unit/Controller/UserDeleteControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Interfaces\UserDeleteInterface;
+use App\Controller\UserDeleteController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserDeleteControllerTest extends TestCase
+{
+    public function testUserAddController() 
+    {
+        $loader = $this->createMock(UserDeleteInterface::class);
+        $loader
+            ->expects($this->once())
+            ->method('deleteUser');
+
+        $classToTest = new UserDeleteController($loader);
+        $id = 5;
+
+        $this->assertInstanceOf(Response::class, $classToTest->deleteOneUser($id));
+    }
+    
+}
