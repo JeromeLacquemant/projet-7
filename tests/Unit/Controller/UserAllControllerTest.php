00edcd3b4787b28427bc222f9744804072c0fa97
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-02-07 15:29:40 +0100

    Implementing the tests for the UserAllController

---
+++ tests/Unit/Controller/UserAllControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Controller\UserAllController;
+use PHPUnit\Framework\TestCase;
+use App\Interfaces\UserAllLoaderInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserAllControllerTest extends TestCase
+{
+    public function testUserAllController() 
+    {
+        $loader = $this->createMock(UserAllLoaderInterface::class);
+        $loader
+            ->expects($this->once())
+            ->method('loadAllUsers');
+
+        $classToTest = new UserAllController($loader);
+
+        $this->assertInstanceOf(Response::class, $classToTest->seeUsers());
+    }
+    
+}
