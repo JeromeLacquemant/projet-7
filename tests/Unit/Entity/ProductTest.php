0a779bc757557534511f11c074871a29bf0f13fe
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-02-14 17:43:40 +0100

    Implementing unit test for the entity Product

---
+++ tests/Unit/Entity/ProductTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use App\Entity\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testSetName()
+    {
+        $classToTest = new Product();
+
+        $classToTest->setName("Produit1");
+        $this->assertEquals('Produit1', $classToTest->getName());
+    }
+
+    public function testSetDescription()
+    {
+        $classToTest = new Product();
+
+        $classToTest->setDescription("Description du produit");
+        $this->assertEquals('Description du produit', $classToTest->getDescription());
+    }
+
+    public function testSetPrice()
+    {
+        $classToTest = new Product();
+
+        $classToTest->setPrice("265");
+        $this->assertEquals('265', $classToTest->getPrice());
+    }
+}
