ed1353e218fb737b44a742b8b66906aa6f39a516
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-02-18 20:23:46 +0100

    Add a file in src which is called CacheKernel.php

---
+++ src/CacheKernel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+
+class CacheKernel extends HttpCache
+{
+    protected function getOptions(): array
+    {
+        return [
+            'default_ttl' => 0,
+        ];
+    }
+}
