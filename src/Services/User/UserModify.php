2dcd80c26c12ec231696f7c831af1696834b78f4
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-02-27 15:51:59 +0100

    Now I can set the authenticated client in the user.

--- src/Services/User/UserModify.php
+++ src/Services/User/UserModify.php
@@ -5,6 +5,7 @@ namespace App\Services\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use App\Services\User\Interfaces\UserModifyInterface;
 
 class UserModify implements UserModifyInterface
@@ -14,24 +15,28 @@ class UserModify implements UserModifyInterface
 
     public function __construct(
         UserRepository $userRepository,
-        EntityManagerInterface $entityManagerInterface
+        EntityManagerInterface $entityManagerInterface,
+        Security $security
         ) 
     {
         $this->userRepository = $userRepository;
         $this->entityManagerInterface = $entityManagerInterface;
+        $this->security = $security;
     }
 
     public function modifyUser($id, Request $request)
     {
         $registeredUser = $this->userRepository->find($id);
-        
+        $client = $this->security->getUser(); 
+
         $data = json_decode($request->getContent(), true);
         //dd($request->getContent());
 
         $registeredUser
             ->setUsername($data["username"])
             ->setPassword($data["password"])
-            ->setEmail($data["email"]);
+            ->setEmail($data["email"])
+            ->setClient($client);
 
         $this->entityManagerInterface->persist($registeredUser);
         $this->entityManagerInterface->flush();
