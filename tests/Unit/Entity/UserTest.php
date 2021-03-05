39c32ab0f2337b794a5aad908e50fe1fdd06c59d
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-03-05 22:28:33 +0100

    Implementing test for user. OK

--- tests/Unit/Entity/UserTest.php
+++ tests/Unit/Entity/UserTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Entity;
 
 use App\Entity\User;
+use App\Entity\Client;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
@@ -31,5 +32,14 @@ class UserTest extends TestCase
 
         $classToTest->setEmail("john@gmail.com");
         $this->assertEquals('john@gmail.com', $classToTest->getEmail());
+    }
+
+    public function testSetClient()
+    {
+        $classToTest = new User();
+        $client = new Client();
+
+        $classToTest->setClient($client);
+        $this->assertEquals($client, $classToTest->getClient());
     }
 }
