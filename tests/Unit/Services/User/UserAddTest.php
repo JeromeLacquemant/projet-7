1e78537a23b8f21f0f0b2e7fc71ea7590be16ea3
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-03-05 21:57:45 +0100

    Implementing tests for UserAdd. OK

--- tests/Unit/Services/User/UserAddTest.php
+++ tests/Unit/Services/User/UserAddTest.php
@@ -4,21 +4,34 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Services\User;
 
+use App\Entity\User;
+use App\Entity\Client;
 use App\Services\User\UserAdd;
 use PHPUnit\Framework\TestCase;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserAddTest extends TestCase
 {
     public function testUserAdd()
     {
+        $client = new Client();
+
+        $user = new User();
+            $user->setUsername("Daniel");
+            $user->setPassword("password");
+            $user->setEmail("mai@mail.com");
+            $user->setClient($client);
+
         $serializer = $this->createMock(SerializerInterface::class);
         $serializer
             ->expects($this->once())
             ->method('deserialize')
-            ->willReturn('App\Entity\User');
+            ->willReturn('test')
+            ->willReturn($user);
 
         $entityManager = $this->createMock(EntityManagerInterface::class);
         $entityManager
@@ -28,9 +41,25 @@ class UserAddTest extends TestCase
             ->expects($this->once())
             ->method('flush');
 
-        $request = $this->createMock(Request::class);
+        $validator = $this->createMock(ValidatorInterface::class);
+        $validator
+            ->expects($this->once())
+            ->method('validate')
+            ->willReturn([]);
 
-        $classToTest = new UserAdd($serializer, $entityManager);
+        $request = $this->createMock(Request::class);
+        $request    
+            ->expects($this->once())
+            ->method('getContent')
+            ->willReturn('{"username":"daniel","password":"password","email":"mail@mail.com"}');
+
+        $security = $this->createMock(Security::class);
+        $security
+            ->expects($this->once())
+            ->method('getUser')
+            ->willReturn($client);
+
+        $classToTest = new UserAdd($serializer, $entityManager, $security, $validator);
 
         $this->assertTrue($classToTest->addUser($request));
     }
