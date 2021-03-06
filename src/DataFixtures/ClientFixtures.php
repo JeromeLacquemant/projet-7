a64534c9bbff5d8d135c37ea6b913a115e77633e
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-03-06 17:23:29 +0100

    Amelioration of the fixtures about the client (for loop)

--- src/DataFixtures/ClientFixtures.php
+++ src/DataFixtures/ClientFixtures.php
@@ -10,26 +10,23 @@ class ClientFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $user = new Client();
-        $user
-            ->setUsername("Client n°1")
-            ->setPassword(password_hash('12345', PASSWORD_BCRYPT))
-            ->setEmail("client1@gmail.com");
-        $manager->persist($user);
+        $names = [
+            'Orange',
+            'Free',
+            'SFR',
+            'Bouygues'
+        ];
 
-        $user = new Client();
-        $user
-            ->setUsername("Client n°2")
-            ->setPassword(password_hash('34521', PASSWORD_BCRYPT))
-            ->setEmail("client2@gmail.com");
-        $manager->persist($user);
+        for ($i = 0; $i <sizeof($names); ++$i) {
+            $client = new Client();
+            $client
+                ->setUsername("Client ".$i)
+                ->setPassword(password_hash('12345', PASSWORD_BCRYPT))
+                ->setEmail("client_".$i."@gmail.com");
 
-        $user = new Client();
-        $user
-            ->setUsername("Client n°3")
-            ->setPassword(password_hash('54321', PASSWORD_BCRYPT))
-            ->setEmail("client3@gmail.com");
-        $manager->persist($user);
+            $manager->persist($client);
+            $this->addReference('Client '.$i, $client);
+        }
 
         $manager->flush();
     }
