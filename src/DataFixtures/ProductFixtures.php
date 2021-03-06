cedd243055826ec52c0a2b04d00308dcedc15170
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-03-06 17:23:10 +0100

    Amelioration of the fixture about the phones (for loop)

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -10,26 +10,15 @@ class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $product = new Product();
-        $product
-            ->setName("Téléphone 1")
-            ->setDescription("Description du téléphone 1")
-            ->setPrice("600");
-        $manager->persist($product);
-
-        $product = new Product();
-        $product
-            ->setName("Téléphone 2")
-            ->setDescription("Description du téléphone 2")
-            ->setPrice("500");
-        $manager->persist($product);
-
-        $product = new Product();
-        $product
-            ->setName("Téléphone 3")
-            ->setDescription("Description du téléphone 3")
-            ->setPrice("300");
-        $manager->persist($product);
+        for ($i = 1; $i<10; ++$i)
+        {
+            $product = new Product();
+            $product
+                ->setName("Téléphone ".$i)
+                ->setDescription("Description du téléphone ".$i)
+                ->setPrice(rand(400, 850));
+            $manager->persist($product);   
+        }
         
         $manager->flush();
     }
