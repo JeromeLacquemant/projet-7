c6bc180bd5be6dbc157ba74ed8bc82ec34af710c
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-03-22 19:56:40 +0100

    Creation of the forgottent unit test ClientOneLoaderTest

---
+++ tests/Unit/Services/Client/ClientOneLoaderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Services\Product;
+
+use App\Entity\Client;
+use PHPUnit\Framework\TestCase;
+use App\Repository\ClientRepository;
+use App\Services\Client\ClientOneLoader;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ClientOneLoaderTest extends TestCase
+{
+    public function testUserOneLoader()
+    {
+        $client = new Client();
+        
+        $clientRepository = $this->createMock(ClientRepository::class);
+            $clientRepository
+                ->expects($this->once())
+                ->method('find')
+                ->willReturn($client);   
+
+        $serializer = $this->createMock(SerializerInterface::class);
+        $serializer
+            ->expects($this->once())
+            ->method('serialize')
+            ->willReturn('{test}');
+
+        $classToTest = new ClientOneLoader($clientRepository, $serializer);
+
+        $id = 8;
+
+        $this->assertIsString($classToTest->loadOneClient($id));
+    }
+}
