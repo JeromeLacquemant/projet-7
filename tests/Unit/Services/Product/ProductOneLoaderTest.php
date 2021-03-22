324842c29101450605f6b0bab4a65c3691ef8c8e
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-03-22 19:53:19 +0100

    Suppression of use which are usefull

--- tests/Unit/Services/Product/ProductOneLoaderTest.php
+++ tests/Unit/Services/Product/ProductOneLoaderTest.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Services\Product;
 
-use App\Entity\Client;
 use App\Entity\Product;
 use PHPUnit\Framework\TestCase;
-use App\Repository\UserRepository;
-use App\Services\User\UserOneLoader;
 use App\Repository\ProductRepository;
 use App\Services\Product\ProductOneLoader;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductOneLoaderTest extends TestCase
