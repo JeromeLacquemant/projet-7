860d3b8444b861607e06d59edd6388bab43867a2
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-03-25 20:46:45 +0100

    Installation of nelmio with the commande symfony composer require nelmio/api-doc-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
