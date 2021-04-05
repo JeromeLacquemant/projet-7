314ba2ec5e25033f035f04621d733b2644aef398
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-04-05 19:50:40 +0200

    Amelioration of the Responder (with the cache)

--- src/Responder/JsonResponder.php
+++ src/Responder/JsonResponder.php
@@ -3,11 +3,16 @@ declare(strict_types=1);
 
 namespace App\Responder;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 final class JsonResponder
 {
+    const codeHttpOk = HttpFoundationResponse::HTTP_OK;
+    const cacheTiming = 3600;
+
     private SerializerInterface $serializer;
 
     public function __construct(SerializerInterface $serializer)
@@ -15,14 +20,22 @@ final class JsonResponder
         $this->serializer = $serializer;
     }
 
-    public function respond($response, $codeHttp)
+    public function respond($response, $codeHttp=self::codeHttpOk, $request)
     {        
-    return
-        new JsonResponse(
+    $jsonResponse = new JsonResponse(
             $this->serializer->serialize($response, 'json'),
             $codeHttp,
             [],
             true
         );
+
+    $jsonResponse->headers->set('Content-Type', 'application/json');
+
+    if($request->isMethodCacheable() == "GET")
+    {
+        $jsonResponse->setMaxAge(self::cacheTiming);
+    }
+    
+    return $jsonResponse;
     }
 }
