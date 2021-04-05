ee647068e190db615806177786afc0151389a023
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-04-05 20:33:13 +0200

    Missing on the test for ProductAllLoader. The unit test is ok now.

--- src/Services/Product/ProductAllLoader.php
+++ src/Services/Product/ProductAllLoader.php
@@ -4,20 +4,16 @@ namespace App\Services\Product;
 
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\SerializerInterface;
 use App\Services\Product\Interfaces\ProductAllLoaderInterface;
 
 class ProductAllLoader implements ProductAllLoaderInterface
 {
     private $productRepository;
-    private $serializer;
 
     public function __construct(
-        ProductRepository $productRepository,
-        SerializerInterface $serializer) 
+        ProductRepository $productRepository) 
     {
         $this->productRepository = $productRepository;
-        $this->serializer = $serializer;
     }
 
     public function loadAllProducts(Request $request){
