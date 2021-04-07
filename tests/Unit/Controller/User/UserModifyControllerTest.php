54c08dec904294d789b04b2b39ee67d442610f22
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-04-07 22:55:46 +0200

    Modification of unit test. It works.

--- tests/Unit/Controller/User/UserModifyControllerTest.php
+++ tests/Unit/Controller/User/UserModifyControllerTest.php
@@ -3,9 +3,11 @@
 namespace App\Tests\Unit\User;
 
 use PHPUnit\Framework\TestCase;
+use App\Responder\JsonResponder;
 use App\Controller\User\UserModifyController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Services\User\Interfaces\UserModifyInterface;
 
 class UserModifyControllerTest extends TestCase
@@ -16,10 +18,16 @@ class UserModifyControllerTest extends TestCase
         $userModifyInterface
             ->expects($this->once())
             ->method('modifyUser');
-            
+        
         $request = $this->createMock(Request::class);
+            
+        $jsonResponder = $this->createMock(JsonResponder::class);
+        $jsonResponder
+            ->expects($this->once())
+            ->method('respond')
+            ->willReturn(new JsonResponse);
 
-        $classToTest = new UserModifyController($userModifyInterface);
+        $classToTest = new UserModifyController($userModifyInterface, $jsonResponder);
         $id = 5;
         
         $this->assertInstanceOf(Response::class, $classToTest->modifyOneUser($id, $request));
