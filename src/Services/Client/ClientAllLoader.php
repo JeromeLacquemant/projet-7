b7717af8a5fff78d1b7f306a4c8ad0f66126e867
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-04-18 01:05:34 +0200

    LinksConstrucor implementing for ClientAllLoader + UnitTest

--- src/Services/Client/ClientAllLoader.php
+++ src/Services/Client/ClientAllLoader.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Client;
 
+use App\Hateos\LinksConstructor;
 use App\Repository\ClientRepository;
 use Symfony\Component\HttpFoundation\Request;
 use App\Services\Client\Interfaces\ClientAllLoaderInterface;
@@ -11,9 +12,11 @@ class ClientAllLoader implements ClientAllLoaderInterface
     private $clientRepository;
 
     public function __construct(
-        ClientRepository $clientRepository) 
+        ClientRepository $clientRepository,
+        LinksConstructor $linksConstructor) 
     {
         $this->clientRepository = $clientRepository;
+        $this->linksConstructor = $linksConstructor;
     }
 
     public function loadAllClients(Request $request){
@@ -22,7 +25,10 @@ class ClientAllLoader implements ClientAllLoaderInterface
         if(!isset($page)) {
             $page = 1;
         }
+
 		$paginatedResult = $this->clientRepository->getClients($page);
+
+        $this->linksConstructor->linksConstruction($paginatedResult, $request);
 
         return $paginatedResult;
     }
