022f482992cb423e54ebe5704181d5901309fa4a
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-04-22 22:32:01 +0200

    Correction of unit tests for UserAllLoader

--- tests/Unit/Services/User/UserAllLoaderTest.php
+++ tests/Unit/Services/User/UserAllLoaderTest.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Services\User;
 
 use PHPUnit\Framework\TestCase;
-use App\Hateos\LinksConstructor;
 use App\Repository\UserRepository;
 use App\Services\User\UserAllLoader;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\InputBag;
+use App\Output\OutputConstructors\UserAllOutputConstruction;
+use App\Output\Outputs\UserOutput;
 
 class UserAllLoaderTest extends TestCase
 {
     public function testUserAllLoader()
     {
+        $userOutput = new UserOutput();
+
         $userRepository = $this->createMock(UserRepository::class);
         $userRepository
             ->expects($this->once())
@@ -26,10 +28,14 @@ class UserAllLoaderTest extends TestCase
         $request = $this->createMock(Request::class);
         $request->query = new InputBag;
 
-        $linksConstructor = $this->createMock(LinksConstructor::class);
+        $userAllOutputConstruction = $this->createMock(UserAllOutputConstruction::class);
+        $userAllOutputConstruction
+            ->expects($this->once())
+            ->method('outputConstruction')
+            ->willReturn($userOutput); 
 
-        $classToTest = new UserAllLoader($userRepository, $linksConstructor);
+        $classToTest = new UserAllLoader($userRepository, $userAllOutputConstruction);
 
-        $this->assertInstanceOf(Paginator::class, $classToTest->loadAllUsers($request));
+        $this->assertEquals($userOutput, $classToTest->loadAllUsers($request));
     }
 }
