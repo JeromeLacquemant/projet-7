8daf3483a2db55df5884c56d76f80c4eb46b9429
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2021-04-22 22:31:42 +0200

    Correction of unit tests for ClientAllLoader

--- tests/Unit/Services/Client/ClientAllLoaderTest.php
+++ tests/Unit/Services/Client/ClientAllLoaderTest.php
@@ -5,30 +5,35 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Services\Client;
 
 use PHPUnit\Framework\TestCase;
-use App\Hateos\LinksConstructor;
 use App\Repository\ClientRepository;
 use App\Services\Client\ClientAllLoader;
-use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\InputBag;
+use App\Output\OutputConstructors\ClientAllOutputConstruction;
+use App\Output\Outputs\ClientOutput;
 
 class ClientAllLoaderTest extends TestCase
 {
     public function testProductAllLoader()
     {
+        $clientOutput = new ClientOutput();
+
         $clientRepository = $this->createMock(ClientRepository::class);
         $clientRepository
             ->expects($this->once())
-            ->method('getClients')
-            ->willReturn(new Paginator(1));
+            ->method('getClients');
 
         $request = $this->createMock(Request::class);
         $request->query = new InputBag;
 
-        $linksConstructor = $this->createMock(LinksConstructor::class);
+        $clientAllOutputConstruction = $this->createMock(ClientAllOutputConstruction::class);
+        $clientAllOutputConstruction
+            ->expects($this->once())
+            ->method('outputConstruction')
+            ->willReturn($clientOutput); 
 
-        $classToTest = new ClientAllLoader($clientRepository, $linksConstructor);
+        $classToTest = new ClientAllLoader($clientRepository, $clientAllOutputConstruction);
 
-        $this->assertInstanceOf(Paginator::class, $classToTest->loadAllClients($request));
+        $this->assertEquals($clientOutput, $classToTest->loadAllClients($request));
     }
 }
